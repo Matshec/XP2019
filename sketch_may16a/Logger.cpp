5d671c2c8322ff5011e8707027a4714d25fc4a43
Didans <dawid.ns@hotmail.com>
2019-05-16 15:15:29 +0200

    Add logger

---
+++ sketch_may16a/Logger.cpp
@@ -0,0 +1,37 @@
+﻿// Logger.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class Logger {
+	
+	// Member Functions() 
+public:
+	void printLog(string message)
+	{
+		time_t now = time(0);
+
+		// convert now to string form
+		char* dt = ctime(&now);
+
+		cout << "Date: " << dt << "Message: " << message << endl;
+	}
+};
+
+int main() {
+
+	// Declare an object of class geeks 
+	Logger obj1;
+
+	// accessing data member 
+	
+
+	// accessing member function 
+	obj1.printLog("elo");
+	return 0;
+}
+
