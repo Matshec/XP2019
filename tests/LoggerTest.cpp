133423dd9e53cb4ac52b79299ef42550c7261e32
Didans <dawid.ns@hotmail.com>
2019-05-16 15:35:30 +0200

    Add logger tests

---
+++ tests/LoggerTest.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "../sketch_may16a/Logger.cpp"
+
+TEST(WithString, TestName) {
+	Logger log;
+	log.printLog("mess");
+	EXPECT_TRUE(true);
+}
+
+TEST(EmptyString, TestName) {
+	Logger log;
+	string s;
+	log.printLog(s);
+	EXPECT_TRUE(true);
+}
